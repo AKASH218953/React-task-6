25f41be314c819d2cefaeabed2703d085b2ff04c
Akash <97045163+AKASH218953@users.noreply.github.com>
2023-10-24 01:02:14 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Anumolu Akash </h1>
+      <div className="tile">
+        <h3>MERN Fullstack devloper</h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
